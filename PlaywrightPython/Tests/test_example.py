863bcbe78f4ecf7a262cdd940bb39af093545d44
Hassan <h.imhah@groupeonepoint.com>
2025-03-18 14:57:37 +0100

    Orangehrm login logout

--- PlaywrightPython/Tests/test_example.py
+++ PlaywrightPython/Tests/test_example.py
@@ -1,7 +1,7 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page, authenticated_context):
+def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
 
     # Expect a title "to contain" a substring.
